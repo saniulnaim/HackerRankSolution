690c36b0d65b330aee1cbdfa1a1f2b23ad7755fc
saniulnaim <saniul.naim@digitech-labs.com>
2019-05-16 12:18:29 +0600

    Add project files.

---
+++ HackerRankSolution/CountingValleys.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HackerRankSolution
+{
+    public static class CountingValleys
+    {
+        public static int CountingValley(int n, string s)
+        {
+            int[] array = new int[n];
+            int levelCount = 0;
+            int numOfValleysCount = 0;
+
+            foreach(char c in s)
+            {
+                if (c == 'U')
+                    levelCount++;
+                if (c == 'D')
+                    levelCount--;
+
+                if (levelCount == 0 && c == 'U')
+                    numOfValleysCount++;
+            }
+
+            return numOfValleysCount;
+        }
+    }
+}
---
+++ HackerRankSolution/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HackerRankSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Interview Preparation KitWarm-up ChallengesCounting Valleys
+            Console.WriteLine(CountingValleys.CountingValley(10, "UDUUUDUDDD"));
+
+            Console.ReadKey();
+        }
+    }
+}
